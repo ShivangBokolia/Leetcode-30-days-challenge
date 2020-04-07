0d8d7c32b45a798e7bccc5ec062e9cdde3fcfbc4
Shivang Bokolia <sb8392@g.rit.edu>
2020-04-07 09:59:42 -0400

    Add files via upload

---
+++ Counting Elements.py
@@ -0,0 +1,14 @@
+def countElements(arr):
+    """ The time complexity reduces if the set is included because repeated elements are not allowed in sets.
+        This would not work if the elements are counted for repeated count.
+        e.g. [1, 1, 2, 2] has the output 2, but if the set is used the output might be 1."""
+    #setarr = set(arr)
+    """ This is one way to do it, but the time complexity is O(n)"""
+    count = 0
+    for i in arr:
+        if (i+1) in arr:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    print(countElements([1, 1, 2, 2]))
